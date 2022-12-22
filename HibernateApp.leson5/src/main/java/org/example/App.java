42439cb63dd969fa8d2a9d40ad606003f2ce3136
Evgeniy Dautov <evg.dautov@mail.ru>
2022-12-23 01:01:35 +0300

    Демонстрация работы с HQL запросами(вывод списка всех людей, вывод списка по условиям, обновление, удаление)

--- HibernateApp.leson5/src/main/java/org/example/App.java
+++ HibernateApp.leson5/src/main/java/org/example/App.java
@@ -5,9 +5,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -16,8 +18,31 @@ public class App {
         try {
             session.beginTransaction();
 
+//            Вывод списка всех людей из БД в консоль
+//            List<Person> people = session.createQuery("FROM Person").getResultList();
+//            for (Person person : people) {
+//                System.out.println(person);
+//            }
+
+//            Вывод списка всех людей, чей возраст > 30
+//            List<Person> people = session.createQuery("FROM Person WHERE age > 30").getResultList();
+//            for (Person person : people) {
+//                System.out.println(person);
+//            }
+
+//            Вывод всех людей, чье имя начинается на 'T'
+//            List<Person> people = session.createQuery("FROM Person WHERE name LIKE 'T%'").getResultList();
+//            for (Person person : people) {
+//                System.out.println(person);
+//            }
 
 
+//            Обновить имена людей чей возраст < 30 на имя "Test"
+//            session.createQuery("UPDATE Person SET name='Test' WHERE age < 30").executeUpdate();
+
+
+//            Удалить людей, чей возраст < 30
+            session.createQuery("DELETE FROM Person WHERE age < 30").executeUpdate();
 
 
             session.getTransaction().commit();
