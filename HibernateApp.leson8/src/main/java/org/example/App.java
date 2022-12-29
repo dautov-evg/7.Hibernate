01553f93b6064b2943076b28b625e2a73efed3f8
Evgeniy Dautov <evg.dautov@mail.ru>
2022-12-29 23:10:10 +0300

    Lesson 8 start

---
+++ HibernateApp.leson8/src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.example.model.Item;
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
