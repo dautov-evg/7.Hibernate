6de09176055ea6aebb0775ed11e8c844a3813f56
Evgeniy Dautov <evg.dautov@mail.ru>
2023-01-02 19:08:51 +0300

    Lesson 9 - Работа с OneToOne

--- HibernateApp.lesson9/src/main/java/org/example/App.java
+++ HibernateApp.lesson9/src/main/java/org/example/App.java
@@ -18,13 +18,30 @@ public class App {
 
         try {
             session.beginTransaction();
-//            создаем нового челоека и новый паспорт
-            Person person = new Person("Test person", 25);
-            Passport passport = new Passport(12345);
-//            устанавливаем связь с двух сторон чтобы в кэш Hibernate была правильная информация
-            person.setPassport(passport);
-//            сохраняем в БД
-            session.save(person);
+////            создаем нового челоека и новый паспорт
+//            Person person = new Person("Test person", 25);
+//            Passport passport = new Passport(12345);
+////            устанавливаем связь с двух сторон чтобы в кэш Hibernate была правильная информация
+//            person.setPassport(passport);
+////            сохраняем в БД
+//            session.save(person);
+
+////            Получаем номер паспорта через человека
+//            Person person = session.get(Person.class, 1);
+//            System.out.println(person.getPassport().getPassportNumber());
+
+
+////            Получаем паспорт из БД и по паспорту получаем человека
+//            Passport passport = session.get(Passport.class, 1);
+//            System.out.println(passport.getPerson());
+
+////            Изменение номера паспорта у человека
+//            Person person = session.get(Person.class,1);
+//            person.getPassport().setPassportNumber(77777);
+
+//            Удаление человека и проверка каскадного удаления паспорта
+            Person person = session.get(Person.class, 1);
+            session.remove(person);
 
             session.getTransaction().commit();
         } finally {
